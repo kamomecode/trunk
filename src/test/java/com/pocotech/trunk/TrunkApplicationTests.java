3993ccb95d512ad7352bb83472453b67ecda1409
ita-jp <28795978+ita-jp@users.noreply.github.com>
2023-10-26 16:22:31 +0900

    spring initlizr

---
+++ src/test/java/com/pocotech/trunk/TrunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocotech.trunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
